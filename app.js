6b5d01da3fdbbba7a36a25c6b0e18d069100991f
SebMunZ <s.alonsomza@gmail.com>
2022-10-11 22:46:38 -0300

    V1.1.x Agregado readme y funcionalidad JS

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
-console.log("Hola, soy la consola")
+console.log("Hola, soy la consola");
 
+/* TODO:
+    -Mejorar funcionalidad (botón de quitar producto)
+    -Agregar resumen de compra al botón de finalizar
+    -Mejorar presentación del documento HTML
+    -Embellecer
+*/
+
+// para seleccionar elementos del DOM
 const carro = document.getElementById("carro");
-const templateLista = document.getElementById("templateLista");
-const fragment = document.createDocumentFragment();
 const botones = document.querySelectorAll(".card .btn");
 
-const carritoObjetos = {}
-const agregarCarrito = (e) => {console.log(e.target.dataset.info);
-    const productos = {
-        titulo: e.target.dataset.info,
-        id: e.target.dataset.info,
-        cantidad: 1   
-    }
-    carritoObjetos[productos.titulo] = productos;
+// para utilizar el template del documento
+const templateLista = document.getElementById("templateLista");
 
-    console.log(carritoObjetos);
+// fragmento para evitar reflow
+const fragment = document.createDocumentFragment();
 
+const carritoObjetos = {};
 
+/* Receptor del evento click en el botón, crea un objeto (productos) usando el dataset,
+acciona la función imprimirCarro, el if utiliza el título leído del dataset para sumar cantidad.
+*/
+const agregarCarrito = (e) => {
+  const productos = {
+    titulo: e.target.dataset.info,
+    id: e.target.dataset.info,
+    cantidad: 1,
+  };
 
-}
+  if (carritoObjetos.hasOwnProperty(productos.titulo)) {
+    productos.cantidad = carritoObjetos[productos.titulo].cantidad + 1;
+  }
 
+  carritoObjetos[productos.titulo] = productos;
+  imprimirCarro(productos);
+};
+
+/* función imprimirCarro.
+Imprime el template sacado del html para agregarlo al DOM.
+Utiliza un forEach para iterar por cada botón, agregando dicho elemento al documento.
+Utilizamos clone para evitar problemas cuando modificamos el template.
+Para evitar que se agregue sin limpiar, agregamos un textContent vacío.
+*/
+const imprimirCarro = () => {
+  carro.textContent = "";
+
+  Object.values(carritoObjetos).forEach((i) => {
+    const clone = templateLista.content.firstElementChild.cloneNode(true);
+    clone.querySelector(".lead").textContent = i.titulo;
+    clone.querySelector(".badge").textContent = i.cantidad;
+
+    fragment.appendChild(clone);
+  });
+
+  carro.appendChild(fragment);
+};
+
+// Evento de clickear el botón
 botones.forEach((btn) => btn.addEventListener("click", agregarCarrito));
---
+++ readme.md
@@ -0,0 +1,10 @@
+#### Practicando JS
+
+Refuerzo personal sobre JS.
+Generando un carro de compras, escalándolo de a poco.
+
+
+## Orden de trabajo ##
+1. Diseño básico en html + bootstrap
+2. Funcionalidad con JS
+3. REALIZANDO: Mejorando JS de a poco.
